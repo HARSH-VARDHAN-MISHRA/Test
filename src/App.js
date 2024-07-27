f8a9a90f6de9d783bb5817d82add0e69e28c8f4a
Harsh Vardhan Mishra <138967484+HARSH-VARDHAN-MISHRA@users.noreply.github.com>
2024-07-27 10:55:39 +0530

    done test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to VPS Hosting</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Empower your development with our top-notch VPS hosting solutions. Get started with ease and flexibility.
         </p>
+        <button className="App-button">
+          Get Started
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn More
         </a>
       </header>
     </div>
